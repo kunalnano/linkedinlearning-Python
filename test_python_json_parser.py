e175790a8c80b31cfaf1065d61761e58f8256d0d
Kunal Sharma <kunalnano@gmail.com>
2019-01-11 10:30:00 -0800

    Delete test_python_json_parser.py

--- test_python_json_parser.py
+++
@@ -1,7 +0,0 @@
-import json
-import urllib
-
-def main():
-    urldata = ""https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson""
-
-    webUrl = urllib.request.urlopen(urldata)
